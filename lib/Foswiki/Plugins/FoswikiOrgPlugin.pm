020a60d9c16a8dac81d14b7e6972455b62a63310
George Clark <geonwiki@fenachrone.com>
2014-11-23 18:49:30 -0500

    Item13107: Bump version

--- lib/Foswiki/Plugins/FoswikiOrgPlugin.pm
+++ lib/Foswiki/Plugins/FoswikiOrgPlugin.pm
@@ -9,8 +9,8 @@ use Foswiki::Func;
 
 use constant TRACE => 1;
 
-our $VERSION           = '1.01';
-our $RELEASE           = '1.01';
+our $VERSION           = '1.02';
+our $RELEASE           = '1.02';
 our $SHORTDESCRIPTION  = 'Adds github WebHook to accept push notifications';
 our $NO_PREFS_IN_TOPIC = 1;
 
